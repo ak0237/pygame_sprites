5afa60f0b9092b8c1258e86780216382a60486ec
andrey <akarv@pop-os.localdomain>
2022-02-22 02:40:25 -0300

    Inicio

---
+++ index.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+
+largura = 640
+altura = 480
+
+## Cores
+PRETO = (0,0,0)
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Sprites')
+
+class Sapo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('sprites/attack_1.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_2.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_3.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_4.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_5.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_6.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_7.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_8.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_9.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_10.png'))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 100, 100
+
+todas_as_sprites = pygame.sprite.Group()
+sapo = Sapo()
+todas_as_sprites.add(sapo)
+
+while True:
+    ##tela.fill(PRETO)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+    pygame.display.flip()
