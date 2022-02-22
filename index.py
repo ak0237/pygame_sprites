ae9ff848f35b07b04ea914fa4c7d41d36c3e0ba6
andrey <akarv@pop-os.localdomain>
2022-02-22 02:54:23 -0300

    Rebit

--- index.py
+++ index.py
@@ -35,17 +35,37 @@ class Sapo(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = 100, 100
 
+        self.animar = False
+
+    def atacar (self):
+        self.animar = True 
+
+    def update(self):
+        if self.animar == True:
+            self.atual += 0.5
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+                self.animar = False
+            self.image = self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+
 todas_as_sprites = pygame.sprite.Group()
 sapo = Sapo()
 todas_as_sprites.add(sapo)
 
+relogio = pygame.time.Clock()
+
 while True:
-    ##tela.fill(PRETO)
+    relogio.tick(30)
+    tela.fill(PRETO)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
+        if event.type == KEYDOWN:
+            sapo.atacar()
+
     todas_as_sprites.draw(tela)
     todas_as_sprites.update()
     pygame.display.flip()
